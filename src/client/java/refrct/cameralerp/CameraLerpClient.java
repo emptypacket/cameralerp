efe3eebc35a27cc8fad01e5f6329f05c7378ff4e
refrct <prettycoolemailname@protonmail.com>
2024-03-19 18:59:15 +0500

    removed non-client entry-point

--- src/client/java/refrct/cameralerp/CameraLerpClient.java
+++ src/client/java/refrct/cameralerp/CameraLerpClient.java
@@ -3,9 +3,9 @@ package refrct.cameralerp;
 import eu.midnightdust.lib.config.MidnightConfig;
 import net.fabricmc.api.ClientModInitializer;
 
-import static refrct.cameralerp.CameraLerp.MOD_ID;
-
 public class CameraLerpClient implements ClientModInitializer {
+	public static final String MOD_ID = "cameralerp";
+
 	@Override
 	public void onInitializeClient() {
 		MidnightConfig.init(MOD_ID, CameraLerpConfig.class);
